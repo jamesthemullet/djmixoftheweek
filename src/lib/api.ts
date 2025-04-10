b411b505c1173e08199acd6b19ad5c825880ff61
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2025-04-10 15:22:21 +0100

    ts fixes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export async function fetchGraphQL(query, variables = {}) {
+export async function fetchGraphQL(query: string, variables = {}) {
   const response = await fetch("https://blog.djmixoftheweek.com/graphql", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,53 @@
 export type Post = {
   slug: string;
-  title: string;
-  seo: { opengraphDescription: string };
-  featuredImage?: { node: { sourceUrl: string } };
+  title: {
+    rendered: string;
+  };
+  seo: {
+    opengraphDescription: string;
+    opengraphImage: {
+      sourceUrl: string;
+    };
+  };
+  featuredImage?: {
+    node: {
+      sourceUrl: string;
+      caption: string;
+      mediaDetails: {
+        width: number;
+        height: number;
+        sizes: {
+          sourceUrl: string;
+          name: string;
+          width: number;
+          height: number;
+        }[];
+      };
+    };
+  };
+  featured_image?: string;
   content: string;
+  genres: {
+    nodes: {
+      name: string;
+      slug: string;
+    }[];
+  };
 } | null;
 
 export type PopularPost = {
   slug: string;
-  title: string;
+  title: {
+    rendered: string;
+  };
   visits: number;
   featuredImage?: { node: { sourceUrl: string } };
+} | null;
+
+export type Genres = {
+  genres: {
+    nodes: Genre[];
+  };
 } | null;
 
 export type Genre = {
@@ -19,3 +56,23 @@ export type Genre = {
   posts: Post[];
   count: number;
 } | null;
+
+export type Comment = {
+  id: string;
+  author: {
+    node: {
+      name: string;
+      avatar: {
+        url: string;
+      };
+    };
+  };
+  date: string;
+  content: {
+    rendered: string;
+  };
+  replies?: Comment[];
+  parentId?: string;
+};
+
+export type Comments = Comment[] | null;
