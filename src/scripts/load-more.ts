820c2b7589a65d8ac3fc51b3c0b43040f89a647f
James Winfield <hello@jameswinfield.co.uk>
2025-05-31 09:59:53 +0100

    prettier fixes

--- src/scripts/load-more.ts
+++ src/scripts/load-more.ts
@@ -1,21 +1,21 @@
-import { fetchGraphQL } from "../lib/api.ts";
-import MORE_POSTS from "../lib/queries/morePosts.ts";
-import type { Post } from "../types.ts";
+import { fetchGraphQL } from '../lib/api.ts';
+import MORE_POSTS from '../lib/queries/morePosts.ts';
+import type { Post } from '../types.ts';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("load-more") as HTMLButtonElement;
-  const postList = document.getElementById("post-list");
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('load-more') as HTMLButtonElement;
+  const postList = document.getElementById('post-list');
 
   const first = 12;
   let cursor = button.dataset.cursor || null;
 
-  button.addEventListener("click", async () => {
+  button.addEventListener('click', async () => {
     if (!cursor) {
-      button.style.display = "none";
+      button.style.display = 'none';
       return;
     }
 
-    button.textContent = "Loading...";
+    button.textContent = 'Loading...';
 
     try {
       const response = await fetchGraphQL(MORE_POSTS, { first, after: cursor });
@@ -25,34 +25,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // biome-ignore lint/complexity/noForEach: <explanation>
       posts.forEach((post: Post) => {
-        const li = document.createElement("li");
+        const li = document.createElement('li');
         li.innerHTML = `
           <a href="${post?.slug}" rel="noopener noreferrer">
             <img src="${
               post?.featuredImage?.node?.mediaDetails?.sizes?.find(
-                (size) => size.name === "medium_large"
-              )?.sourceUrl || ""
+                (size) => size.name === 'medium_large'
+              )?.sourceUrl || ''
             }" alt="" width="768" height="576" loading="lazy" />
           </a>
           <h2>
             <a href="${post?.slug}" rel="noopener noreferrer">${post?.title}</a>
           </h2>
           <ul class="genres">
-            <li>${post?.genres.nodes.map((genre) => genre.name).join(", ")}</li>
+            <li>${post?.genres.nodes.map((genre) => genre.name).join(', ')}</li>
           </ul>
         `;
         postList?.appendChild(li);
       });
 
-      button.dataset.cursor = cursor || "";
-      button.textContent = "Load More";
+      button.dataset.cursor = cursor || '';
+      button.textContent = 'Load More';
 
       if (!cursor) {
-        button.style.display = "none";
+        button.style.display = 'none';
       }
     } catch (error) {
-      console.error("Error loading more posts:", error);
-      button.textContent = "Load More";
+      console.error('Error loading more posts:', error);
+      button.textContent = 'Load More';
     }
   });
 });
