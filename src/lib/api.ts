13f429c14a8a40c265e677edf3e3d2b19ce6483f
James Winfield <hello@jameswinfield.co.uk>
2025-09-15 20:40:55 +0100

    lint fixes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,15 +1,15 @@
 export async function fetchGraphQL(query: string, variables = {}) {
-  const response = await fetch('https://blog.djmixoftheweek.com/graphql', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query, variables }),
-  });
+	const response = await fetch("https://blog.djmixoftheweek.com/graphql", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ query, variables }),
+	});
 
-  const json = await response.json();
+	const json = await response.json();
 
-  if (!response.ok || json.errors) {
-    throw new Error(`GraphQL Error: ${JSON.stringify(json.errors)}`);
-  }
+	if (!response.ok || json.errors) {
+		throw new Error(`GraphQL Error: ${JSON.stringify(json.errors)}`);
+	}
 
-  return json.data;
+	return json.data;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,79 +1,79 @@
 export type Post = {
-  slug: string;
-  title: {
-    rendered: string;
-  };
-  seo: {
-    opengraphDescription: string;
-    opengraphImage: {
-      sourceUrl: string;
-    };
-  };
-  featuredImage?: {
-    node: {
-      sourceUrl: string;
-      srcSet: string;
-      caption: string;
-      mediaDetails: {
-        width: number;
-        height: number;
-        sizes: {
-          sourceUrl: string;
-          name: string;
-          width: number;
-          height: number;
-        }[];
-      };
-    };
-  };
-  featured_image?: string;
-  content: string;
-  genres: {
-    nodes: {
-      name: string;
-      slug: string;
-    }[];
-  };
+	slug: string;
+	title: {
+		rendered: string;
+	};
+	seo: {
+		opengraphDescription: string;
+		opengraphImage: {
+			sourceUrl: string;
+		};
+	};
+	featuredImage?: {
+		node: {
+			sourceUrl: string;
+			srcSet: string;
+			caption: string;
+			mediaDetails: {
+				width: number;
+				height: number;
+				sizes: {
+					sourceUrl: string;
+					name: string;
+					width: number;
+					height: number;
+				}[];
+			};
+		};
+	};
+	featured_image?: string;
+	content: string;
+	genres: {
+		nodes: {
+			name: string;
+			slug: string;
+		}[];
+	};
 } | null;
 
 export type PopularPost = {
-  slug: string;
-  title: {
-    rendered: string;
-  };
-  visits: number;
-  featuredImage?: { node: { sourceUrl: string } };
+	slug: string;
+	title: {
+		rendered: string;
+	};
+	visits: number;
+	featuredImage?: { node: { sourceUrl: string } };
 } | null;
 
 export type Genres = {
-  genres: {
-    nodes: Genre[];
-  };
+	genres: {
+		nodes: Genre[];
+	};
 } | null;
 
 export type Genre = {
-  name: string;
-  slug: string;
-  posts: Post[];
-  count: number;
+	name: string;
+	slug: string;
+	posts: Post[];
+	count: number;
 } | null;
 
 export type Comment = {
-  id: string;
-  author: {
-    node: {
-      name: string;
-      avatar: {
-        url: string;
-      };
-    };
-  };
-  date: string;
-  content: {
-    rendered: string;
-  };
-  replies?: Comment[];
-  parentId?: string;
+	id: string;
+	author: {
+		node: {
+			name: string;
+			avatar: {
+				url: string;
+			};
+		};
+	};
+	date: string;
+	content: {
+		rendered: string;
+	};
+	replies?: Comment[];
+	parentId?: string;
 };
 
 export type Comments = Comment[] | null;
